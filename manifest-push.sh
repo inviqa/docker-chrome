5996749c90e07f22e4210a371dd721bf0a5a7234
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2024-05-16 10:14:47 +0100

    Add support for bookworm chromium
    bullseye chromium became EOL earlier this year, so is being discontinued here

--- manifest-push.sh
+++ manifest-push.sh
@@ -4,7 +4,18 @@ set -e -o pipefail
 
 echo "${DOCKER_REGISTRY_CREDS_PSW}" | docker login --username "${DOCKER_REGISTRY_CREDS_USR}" --password-stdin "${DOCKER_REGISTRY}"
 
-for IMAGE in $(docker compose config --format json | jq -r '.services[].image' | grep -v null); do
+for IMAGE in $(docker compose config --format json | jq -r 'try .services[].image'); do
+    if [ -z "$IMAGE" ] || [ "$IMAGE" = null ]; then
+        continue
+    fi
+    docker manifest create "${IMAGE}" "${IMAGE}-linux-amd64" "${IMAGE}-linux-arm64"
+    docker manifest push "${IMAGE}"
+done
+
+for IMAGE in $(docker compose config --format json | jq -r 'try .services[].build.tags[]'); do
+    if [ -z "$IMAGE" ] || [ "$IMAGE" = null ]; then
+        continue
+    fi
     docker manifest create "${IMAGE}" "${IMAGE}-linux-amd64" "${IMAGE}-linux-arm64"
     docker manifest push "${IMAGE}"
 done
